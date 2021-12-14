5f861d700a14572d2a99da9695d1d688a4c1260f
NVTACSI <tuan.nguyen2017@ict.jvn.edu.vn>
2021-12-14 15:12:01 +0100

    Update the last measurement of longitudinal data

--- competing_methods/all_model.py
+++ competing_methods/all_model.py
@@ -10,11 +10,12 @@ from lifelines.utils import concordance_index as c_index_score
 from time import time
 from lights.simulation import SimuJointLongitudinalSurvival
 import pandas as pd
+from scipy.stats import beta
 def load_data(simu):
     if simu:
         n_long_features = 5
         n_time_indep_features = 10
-        n_samples = 100
+        n_samples = 200
         simu = SimuJointLongitudinalSurvival(seed=123,
                                              n_long_features=n_long_features,
                                              n_samples=n_samples,
@@ -33,26 +34,44 @@ def load_data(simu):
         df1 = pd.DataFrame(data=np.column_stack((id, T, delta, X)),
                            columns=["id", "T_survival", "delta"] +
                                    time_indep_feat)
+        # generate t_max
+        a = 2
+        b = 5
+        r = beta.rvs(a, b, size=n_samples)
+        t_max = T * (1 - r)
         for i in range(n_samples):
             Y_i_ = []
             for l in range(n_long_features):
                 Y_il = Y.loc[i][l]
-                y_il = Y_il.values.flatten().tolist()
+                times_il = Y_il.index.values
+                t_long_chosen = (times_il <= t_max[i])
+                if not np.any(t_long_chosen):
+                    t_max[i] = times_il[0]
+                    t_long_chosen = (times_il <= t_max[i])
+                times_il = times_il[t_long_chosen]
+                y_il = Y_il.values.flatten()[t_long_chosen].tolist()
+                n_il = len(times_il)
+                tmp = data_lights.loc[i, time_dep_feat[l]]
+                if tmp[tmp.index.values <= t_max[i]].empty:
+                    data_lights[time_dep_feat[l]][i] = tmp[tmp.index.values == tmp.index.values[0]]
+                    t_max[i] = tmp.index.values[0]
+                else:
+                    data_lights[time_dep_feat[l]][i] = tmp[tmp.index.values <= t_max[i]]
                 Y_i_.append(y_il)
 
-            times_il = Y_il.index.values
-            n_il = len(times_il)
             Y_i = np.column_stack(
-                (np.array([id[i]] * n_il), times_il, np.array(Y_i_).T))
+                (np.array([id[i]] * n_il), times_il, np.array([t_max[i]] * n_il), np.array(Y_i_).T))
             if i == 0:
                 Y_ = Y_i
             else:
                 Y_ = np.row_stack((Y_, Y_i))
-        df2 = pd.DataFrame(data=Y_, columns=["id", "T_long"] + time_dep_feat)
+        data_lights["T_max"] = t_max
+        df2 = pd.DataFrame(data=Y_, columns=["id", "T_long", "T_max"] + time_dep_feat)
         data = pd.merge(df2, df1, on="id")
+
     else:
         # load PBC Seq
-        robjects.r.source(os.getcwd() + "/lights/competing_methods/load_PBC_Seq")
+        robjects.r.source(os.getcwd() + "/competing_methods/load_PBC_Seq.R")
         time_dep_feat = ["serBilir", "SGOT", "albumin"]
         time_indep_feat = ["age", "drug", "sex"]
         data_R = robjects.r["load"]()
@@ -65,17 +84,37 @@ def load_data(simu):
         n_samples = len(id_list)
         n_long_features = len(time_dep_feat)
         data_lights = data.drop_duplicates(subset=["id"])
+
+        # generate t_max
+        a = 2
+        b = 5
+        r = beta.rvs(a, b, size=n_samples)
+        T = data_lights["T_survival"].values
+        t_max = T * (1 - r)
+
         Y = []
+        t_max_R = []
         for i in range(n_samples):
+            tmp = data[(data["id"] == id_list[i]) & (data["T_long"] < t_max[i])]
+            if tmp.empty:
+                t_max[i] = data[(data["id"] == id_list[i])]["T_long"][0]
+                n_i = 1
+            else:
+                n_i = tmp.shape[0]
+            data = data[(data["id"] != id_list[i]) |
+                    ((data["id"] == id_list[i]) & (data["T_long"] < t_max[i]))]
             y_i = []
             for l in range(n_long_features):
                 Y_il = data[["T_long", time_dep_feat[l]]][
-                    data["id"] == id_list[i]]
+                    (data["id"] == id_list[i]) & (data['T_long'] < t_max[i])]
                 # TODO: Add value of 1/365 (the first day of survey instead of 0)
                 y_i += [pd.Series(Y_il[time_dep_feat[l]].values,
                                   index=Y_il["T_long"].values + 1 / 365)]
             Y.append(y_i)
+            t_max_R += [t_max[i]] * n_i
         data_lights[time_dep_feat] = Y
+        data_lights["T_max"] = t_max
+        data["T_max"] = np.array(t_max_R).flatten()
     return (data, data_lights, time_dep_feat, time_indep_feat)
 
 def extract_lights_feat(data, time_indep_feat, time_dep_feat):
@@ -124,7 +163,7 @@ def all_model(n_runs = 1, simu=True):
         data_R_test, T_R_test, delta_R_test = extract_R_feat(data_test)
 
         # The penalized Cox model.
-        robjects.r.source(os.getcwd() + "/lights/competing_methods/CoxNet")
+        robjects.r.source(os.getcwd() + "/competing_methods/CoxNet.R")
         X_R_train = robjects.r["Cox_get_long_feat"](data_R_train, time_dep_feat,
                                                   time_indep_feat)
         X_R_test = robjects.r["Cox_get_long_feat"](data_R_test, time_dep_feat,
@@ -141,7 +180,7 @@ def all_model(n_runs = 1, simu=True):
 
         # Multivariate joint latent class model.
         start = time()
-        robjects.r.source(os.getcwd() + "/lights/competing_methods/MJLCMM")
+        robjects.r.source(os.getcwd() + "/competing_methods/MJLCMM.R")
         trained_long_model, trained_mjlcmm = robjects.r["MJLCMM_fit"](data_R_train,
                                              robjects.StrVector(time_dep_feat),
                                              robjects.StrVector(time_indep_feat),
@@ -156,13 +195,11 @@ def all_model(n_runs = 1, simu=True):
 
         # Multivariate shared random effect model.
         start = time()
-        robjects.r.source(os.getcwd() + "/lights/competing_methods/JMBayes")
+        robjects.r.source(os.getcwd() + "/competing_methods/JMBayes.R")
         trained_JMBayes = robjects.r["fit"](data_R_train,
                                             robjects.StrVector(time_dep_feat),
                                             robjects.StrVector(time_indep_feat))
-        # JMBayes_pred = robjects.r["score"](trained_JMBayes, data_R_test, t_max=4)
-        # JMBayes_marker = np.array(JMBayes_pred.rx2('full.results')[0])
-        JMBayes_marker = np.array(robjects.r["score"](trained_JMBayes, data_R_test, t_max=4))
+        JMBayes_marker = np.array(robjects.r["score"](trained_JMBayes, data_R_test))
         JMBayes_c_index = c_index_score(T_test, JMBayes_marker, delta_test)
         JMBayes_c_index = max(JMBayes_c_index, 1 - JMBayes_c_index)
         t.add_row(["JMBayes", "%g" % JMBayes_c_index, "%.3f" % (time() - start)])
@@ -176,14 +213,13 @@ def all_model(n_runs = 1, simu=True):
                               asso_functions=["lp", "re"],
                               l_pen_SGL=0.02, eta_sp_gp_l1=.9, l_pen_EN=0.02)
         learner.fit(X_lights_train, Y_lights_train, T_train, delta_train)
-        lights_marker = learner.predict_marker(X_lights_test, Y_lights_test)
+        prediction_times = data_lights_test[["T_max"]].values.flatten()
+        lights_marker = learner.predict_marker(X_lights_test, Y_lights_test, prediction_times)
         lights_c_index = c_index_score(T_test, lights_marker, delta_test)
         lights_c_index = max(lights_c_index, 1 - lights_c_index)
         t.add_row(["lights", "%g" % lights_c_index, "%.3f" % (time() - start)])
 
         print(t)
-#
-# all_model(simu=False)
 
 
 
