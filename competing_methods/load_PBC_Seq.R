9c8d89a3e77063f469d7611d86b3f583fc1e03ae
NVTACSI <tuan.nguyen2017@ict.jvn.edu.vn>
2021-12-21 13:38:37 +0100

    Rafactor the PBCSeq loading process

--- competing_methods/load_PBC_Seq.R
+++ competing_methods/load_PBC_Seq.R
@@ -10,14 +10,10 @@ library("JMbayes")
 load <- function() {
 
     data(pbc2, package = "JMbayes")
-    colnames <- names(pbc2)
     data <- pbc2
     data$T_long <- data$year
     data$T_survival <- data$years
     data$delta <- data$status2
-    data$SGOT <- log(data$SGOT)
-    data$serBilir <- log(data$serBilir)
-    data$albumin <- log(data$albumin)
 
     return(data)
 }
